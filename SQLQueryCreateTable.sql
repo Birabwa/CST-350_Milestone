86c5c89294df0a619befc3744a27c5f1708094aa
will knotts <williamwknotts@gmail.com>
2023-08-28 02:24:32 -0700

    Can now log in with DB user and pass/ still need to add the user after registration

---
+++ SQLQueryCreateTable.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Register] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Firstname] NVARCHAR (40) NOT NULL,
+    [Lastname]  NVARCHAR (40) NOT NULL,
+    [Sex]       NVARCHAR (40) NOT NULL,
+    [Age]       NVARCHAR (3)  NOT NULL,
+    [State]     NVARCHAR (40) NOT NULL,
+    [Email]     NVARCHAR (40) NOT NULL,
+    [Username]  NVARCHAR (40) NOT NULL,
+    [Password]  NVARCHAR (40) NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SQLQueryInsertUser.sql
@@ -0,0 +1,1 @@
+INSERT INTO dbo.Register (FIRSTNAME, LASTNAME, SEX, AGE, STATE, EMAIL, USERNAME, PASSWORD) VALUES ('Firstname', 'Lastname', 'Male', 35, 'AZ', 'Email', 'Username', 'Password');
