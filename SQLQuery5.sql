4f94cbe77f3618fb58ff15005da529bf9bbae8ad
will knotts <williamwknotts@gmail.com>
2023-08-28 12:08:05 -0700

    Have everything working except registration insertion.

---
+++ SQLQuery5.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[Register] (FIRSTNAME, LASTNAME, SEX, AGE, STATE, EMAIL, USERNAME, PASSWORD) VALUES ('Firstname', 'Lastname', 'Male', '35', 'AZ', 'Email', 'Username', 'Password');
+
+SELECT * FROM [dbo].[Register]
